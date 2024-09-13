2a9aaa4bd3fb5c258b79540793d67bd84a748361
Payal Bhattamisra <147300476+payalbhattamisra@users.noreply.github.com>
2024-09-13 18:23:36 +0530

    Commit by Necleo

---
+++ src/App.jsx
@@ -0,0 +1,643 @@
+import React from "react";
+
+const GeneratedComponent = () => (
+  <div style={{ margin: 0 }}>
+    <div
+      class=""
+      id="_15_1209_Frame_100"
+      style={{
+        all: "unset",
+        position: "absolute",
+        height: "calc(100vh - 0px - 0px)",
+        width: "calc(100% - 0px - 0px)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        flexWrap: "nowrap",
+        gap: "14px",
+      }}
+    >
+      <div
+        class=""
+        id="_15_1210_Frame_81"
+        style={{
+          all: "unset",
+          position: "relative",
+          borderColor: "#e0e2e9ff",
+          borderStyle: "solid",
+          borderWidth: "0.0px 0.0px 1.0px 0.0px",
+          width: "calc(100% - 0px - 0px)",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexWrap: "nowrap",
+          padding: "0px 0px 14px 0px",
+        }}
+      >
+        <div
+          class=""
+          id="I15_1210_105_802_Frame_72"
+          style={{
+            all: "unset",
+            position: "relative",
+            height: "30.0px",
+            flex: "1",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            flexWrap: "nowrap",
+            gap: "6px",
+          }}
+        >
+          <div
+            class=""
+            id="I15_1210_105_821_Ellipse_10"
+            style={{
+              all: "unset",
+              position: "relative",
+              background: "rgba(51, 206, 26, 1.00)",
+              height: "8.0px",
+              width: "8.0px",
+              borderRadius: "50%",
+              left: "0.0px",
+              top: "11.0px",
+            }}
+          ></div>
+
+          <span
+            id="TO_DO"
+            style={{
+              color: "#000000ff",
+              fontFamily: "Poppins",
+              fontSize: "20.0px",
+              fontWeight: "600",
+              textAlign: "left",
+              position: "relative",
+            }}
+          >
+            COMPLETED
+          </span>
+          <div
+            class=""
+            id="I15_1210_105_804_Frame_71"
+            style={{
+              all: "unset",
+              position: "relative",
+              background: "rgba(255, 255, 255, 1.00)",
+              borderRadius: "15px",
+              height: "-2.0px",
+              width: "-2.0px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              flexWrap: "nowrap",
+              gap: "10px",
+              padding: "10px 10px 10px 10px",
+            }}
+          >
+            <span
+              id="_2"
+              style={{
+                color: "#000000ff",
+                fontFamily: "Poppins",
+                fontSize: "20.0px",
+                fontWeight: "600",
+                textAlign: "left",
+                height: "12.0px",
+                width: "8.0px",
+                position: "relative",
+              }}
+            >
+              3
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class=""
+        id="_15_1211_Frame_89"
+        style={{
+          all: "unset",
+          position: "relative",
+          background: "rgba(255, 255, 255, 1.00)",
+          borderRadius: "16px",
+          boxShadow: "0.0px 4.0px 4.0px 0.0px rgba(0, 0, 0, 0.25)",
+          width: "calc(100% - 16px - 16px)",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexWrap: "nowrap",
+          gap: "10px",
+          padding: "16px 16px 16px 16px",
+        }}
+      >
+        <div
+          class=""
+          id="_15_1212_Frame_98"
+          style={{
+            all: "unset",
+            position: "relative",
+            height: "52.0px",
+            flex: "1",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            flexWrap: "nowrap",
+            gap: "10px",
+          }}
+        >
+          <div
+            class=""
+            id="_15_1213_Frame_99"
+            style={{
+              all: "unset",
+              position: "relative",
+              height: "24.0px",
+              width: "calc(100% - 0px - 0px)",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              flexWrap: "nowrap",
+              gap: "10px",
+            }}
+          >
+            <div
+              class=""
+              id="_15_1214_Frame_85"
+              style={{
+                all: "unset",
+                position: "relative",
+                height: "24.0px",
+                flex: "1",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                flexWrap: "nowrap",
+                gap: "12px",
+              }}
+            >
+              <div
+                class=""
+                id="_15_1215_Frame_84"
+                style={{
+                  all: "unset",
+                  position: "relative",
+                  height: "24.0px",
+                  width: "calc(100% - 0px - 0px)",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-start",
+                  alignItems: "flex-start",
+                  flexWrap: "nowrap",
+                  gap: "10px",
+                }}
+              >
+                <div
+                  class=""
+                  id="_15_1216_Frame_82"
+                  style={{
+                    all: "unset",
+                    position: "relative",
+                    background: "rgba(21, 225, 54, 0.28)",
+                    borderRadius: "12px",
+                    height: "16.0px",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexWrap: "nowrap",
+                    gap: "10px",
+                    padding: "4px 10px 4px 10px",
+                  }}
+                >
+                  <span
+                    id="Mobile_app_Design"
+                    style={{
+                      color: "#111111ff",
+                      fontFamily: "Poppins",
+                      fontSize: "12.0px",
+                      fontWeight: "700",
+                      lineHeight: "16px",
+                      textAlign: "left",
+                      position: "relative",
+                    }}
+                  >
+                    Mobile app Design
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div
+              class=""
+              id="_15_1220_icons"
+              style={{
+                all: "unset",
+                position: "relative",
+                overflow: "hidden",
+                height: "20.0px",
+                width: "20.0px",
+              }}
+            >
+              <img
+                id="_15_1221_Vector__Stroke_"
+                src="assets/images/748940472.svg"
+                alt="Vector (Stroke)"
+                style={{
+                  position: "absolute",
+                  left: "calc(100% * 0.8333333015441895)",
+                  top: "calc(100% * 0.7083333492279053)",
+                }}
+              />
+              <img
+                id="_15_1222_Vector__Stroke_"
+                src="assets/images/4703177543.svg"
+                alt="Vector (Stroke)"
+                style={{
+                  position: "absolute",
+                  left: "calc(100% * 0.8333333015441895)",
+                  top: "calc(100% * 0.41666665077209475)",
+                }}
+              />
+              <img
+                id="_15_1223_Vector__Stroke_"
+                src="assets/images/7172081693.svg"
+                alt="Vector (Stroke)"
+                style={{
+                  position: "absolute",
+                  left: "calc(100% * 0.8333333015441895)",
+                  top: "calc(100% * 0.125)",
+                }}
+              />
+            </div>
+          </div>
+
+          <span
+            id="Second_design_concept"
+            style={{
+              color: "#111111ff",
+              fontFamily: "Poppins",
+              fontSize: "14.0px",
+              fontWeight: "400",
+              lineHeight: "18px",
+              textAlign: "left",
+              position: "relative",
+            }}
+          >
+            Second design concept
+          </span>
+        </div>
+
+        <div
+          class=""
+          id="_15_1226_Frame_92"
+          style={{
+            all: "unset",
+            position: "relative",
+            height: "24.0px",
+            flex: "1",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexWrap: "nowrap",
+          }}
+        >
+          <div
+            class=""
+            id="_15_1227_Frame_90"
+            style={{
+              all: "unset",
+              position: "relative",
+              height: "16.0px",
+              flex: "1",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              flexWrap: "nowrap",
+              gap: "10px",
+            }}
+          >
+            <div
+              class=""
+              id="_15_1228_Frame_88"
+              style={{
+                all: "unset",
+                position: "relative",
+                height: "16.0px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                flexWrap: "nowrap",
+                gap: "4px",
+              }}
+            >
+              <span
+                id="Deadline__10_5_24"
+                style={{
+                  color: "#5a5a5aff",
+                  fontSize: "12.0px",
+                  fontWeight: "null",
+                  lineHeight: "16px",
+                  textAlign: "left",
+                  position: "relative",
+                }}
+              >
+                Deadline: 10/5/24
+              </span>
+            </div>
+          </div>
+
+          <div
+            class=""
+            id="_15_1234_Frame_93"
+            style={{
+              all: "unset",
+              position: "relative",
+              height: "24.0px",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              flexWrap: "nowrap",
+              gap: "6px",
+            }}
+          ></div>
+        </div>
+      </div>
+
+      <div
+        class=""
+        id="_15_1235_Frame_95"
+        style={{
+          all: "unset",
+          position: "relative",
+          width: "calc(100% - 0px - 0px)",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexWrap: "nowrap",
+          gap: "14px",
+        }}
+      >
+        <div
+          class=""
+          id="_15_1236_Frame_90"
+          style={{
+            all: "unset",
+            position: "relative",
+            background: "rgba(255, 255, 255, 1.00)",
+            borderRadius: "16px",
+            boxShadow: "0.0px 4.0px 4.0px 0.0px rgba(0, 0, 0, 0.25)",
+            height: "52.0px",
+            flex: "1",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            flexWrap: "nowrap",
+            gap: "10px",
+            padding: "16px 16px 16px 16px",
+          }}
+        >
+          <div
+            class=""
+            id="_15_1237_Frame_98"
+            style={{
+              all: "unset",
+              position: "relative",
+              height: "52.0px",
+              flex: "1",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              flexWrap: "nowrap",
+              gap: "10px",
+            }}
+          >
+            <div
+              class=""
+              id="_15_1238_Frame_99"
+              style={{
+                all: "unset",
+                position: "relative",
+                height: "24.0px",
+                width: "calc(100% - 0px - 0px)",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                flexWrap: "nowrap",
+                gap: "10px",
+              }}
+            >
+              <div
+                class=""
+                id="_15_1239_Frame_85"
+                style={{
+                  all: "unset",
+                  position: "relative",
+                  height: "24.0px",
+                  flex: "1",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                  alignItems: "flex-start",
+                  flexWrap: "nowrap",
+                  gap: "12px",
+                }}
+              >
+                <div
+                  class=""
+                  id="_15_1240_Frame_84"
+                  style={{
+                    all: "unset",
+                    position: "relative",
+                    height: "24.0px",
+                    width: "calc(100% - 0px - 0px)",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start",
+                    flexWrap: "nowrap",
+                    gap: "10px",
+                  }}
+                >
+                  <div
+                    class=""
+                    id="_15_1241_Frame_82"
+                    style={{
+                      all: "unset",
+                      position: "relative",
+                      background: "rgba(21, 225, 54, 0.28)",
+                      borderRadius: "12px",
+                      height: "16.0px",
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      flexWrap: "nowrap",
+                      gap: "10px",
+                      padding: "4px 10px 4px 10px",
+                    }}
+                  >
+                    <span
+                      id="Design_system"
+                      style={{
+                        color: "#111111ff",
+                        fontFamily: "Poppins",
+                        fontSize: "12.0px",
+                        fontWeight: "700",
+                        lineHeight: "16px",
+                        textAlign: "left",
+                        position: "relative",
+                      }}
+                    >
+                      Design system
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class=""
+                id="_15_1245_icons"
+                style={{
+                  all: "unset",
+                  position: "relative",
+                  overflow: "hidden",
+                  height: "20.0px",
+                  width: "20.0px",
+                }}
+              >
+                <img
+                  id="_15_1246_Vector__Stroke_"
+                  src="assets/images/748940472.svg"
+                  alt="Vector (Stroke)"
+                  style={{
+                    position: "absolute",
+                    left: "calc(100% * 0.8333333015441895)",
+                    top: "calc(100% * 0.7083333492279053)",
+                  }}
+                />
+                <img
+                  id="_15_1247_Vector__Stroke_"
+                  src="assets/images/4703177543.svg"
+                  alt="Vector (Stroke)"
+                  style={{
+                    position: "absolute",
+                    left: "calc(100% * 0.8333333015441895)",
+                    top: "calc(100% * 0.41666665077209475)",
+                  }}
+                />
+                <img
+                  id="_15_1248_Vector__Stroke_"
+                  src="assets/images/7172081693.svg"
+                  alt="Vector (Stroke)"
+                  style={{
+                    position: "absolute",
+                    left: "calc(100% * 0.8333333015441895)",
+                    top: "calc(100% * 0.125)",
+                  }}
+                />
+              </div>
+            </div>
+
+            <span
+              id="Overall_UX_Process_of_full_product_for_first_version"
+              style={{
+                color: "#111111ff",
+                fontFamily: "Poppins",
+                fontSize: "14.0px",
+                fontWeight: "400",
+                lineHeight: "18px",
+                textAlign: "left",
+                position: "relative",
+              }}
+            >
+              Overall UX Process of full product for first version
+            </span>
+          </div>
+
+          <div
+            class=""
+            id="_15_1251_Frame_99"
+            style={{
+              all: "unset",
+              position: "relative",
+              height: "16.0px",
+              flex: "1",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              flexWrap: "nowrap",
+            }}
+          >
+            <div
+              class=""
+              id="_15_1252_Frame_90"
+              style={{
+                all: "unset",
+                position: "relative",
+                height: "16.0px",
+                flex: "1",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                flexWrap: "nowrap",
+                gap: "10px",
+              }}
+            >
+              <div
+                class=""
+                id="_15_1253_Frame_88"
+                style={{
+                  all: "unset",
+                  position: "relative",
+                  height: "16.0px",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  flexWrap: "nowrap",
+                  gap: "4px",
+                }}
+              >
+                <span
+                  id="Deadline__10_5_24"
+                  style={{
+                    color: "#5a5a5aff",
+                    fontSize: "12.0px",
+                    fontWeight: "null",
+                    lineHeight: "16px",
+                    textAlign: "left",
+                    position: "relative",
+                  }}
+                >
+                  Deadline: 10/5/24
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default GeneratedComponent;
